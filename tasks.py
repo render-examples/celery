ea6dab22fe6c3fbf521de740e7a9f2add139d04a
scottnuma <scott.numamoto@gmail.com>
2022-05-02 15:05:15 -0700

    Add logging

--- tasks.py
+++ tasks.py
@@ -1,9 +1,12 @@
 import os
 from celery import Celery
+from celery.utils.log import get_task_logger
 
 app = Celery('tasks', broker=os.getenv("CELERY_BROKER_URL"))
+logger = get_task_logger(__name__)
 
 
 @app.task
 def add(x, y):
+    logger.info(f'Adding {x} + {y}')
     return x + y
